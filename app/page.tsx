1d1f5a2bf819b9999c2344217d9090b57560261e
SP00419-at-675013145414 <Consultant.Parthsaini@nivabupa.com>
2025-02-15 22:39:09 +0530

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,13 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { DndListHandle } from "@/components/DndListHandle/DndListHandle";
-import { Group } from "@mantine/core";
 import { myTodos } from "@/helpers/content";
-import { Todo } from "@/helpers/types";
 
 export default function Home() {
-  const completedTodos = myTodos.filter((todo: Todo) => todo.completed);
-  const inCompleteTodos = myTodos.filter((todo: Todo) => !todo.completed);
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Group gap="xl">
-          <DndListHandle todos={completedTodos} />
-          <DndListHandle todos={inCompleteTodos} />
-        </Group>
+        <DndListHandle todos={myTodos} />
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
--- components/DndListHandle/DndListHandle.tsx
+++ components/DndListHandle/DndListHandle.tsx
@@ -1,18 +1,29 @@
 "use client";
 
-import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import {
+  DragDropContext,
+  Draggable,
+  DraggableLocation,
+  Droppable,
+} from "@hello-pangea/dnd";
 import { IconGripVertical } from "@tabler/icons-react";
 import cx from "clsx";
-import { Text } from "@mantine/core";
+import { Group, Paper, Stack, Text, Title } from "@mantine/core";
 import { useListState } from "@mantine/hooks";
+import type { UseListStateHandlers } from "@mantine/hooks";
 import classes from "./DndListHandle.module.css";
 import { Todo } from "@/helpers/types";
 
 export function DndListHandle({ todos }: { todos: Todo[] }) {
-  const [state, handlers] = useListState(todos);
+  const [completedTodos, completedTodoshandlers] = useListState(
+    todos.filter((todo: Todo) => todo.completed)
+  );
+  const [inCompleteTodos, inCompleteTodoshandlers] = useListState(
+    todos.filter((todo: Todo) => !todo.completed)
+  );
 
-  const items = state.map((item, index) => (
-    <Draggable key={item.id} index={index} draggableId={item.id.toString()}>
+  const completedTodosItems = completedTodos.map((item, index) => (
+    <Draggable key={item.todo} index={index} draggableId={item.todo}>
       {(provided, snapshot) => (
         <div
           className={cx(classes.item, {
@@ -25,27 +36,113 @@ export function DndListHandle({ todos }: { todos: Todo[] }) {
             <IconGripVertical size={18} stroke={1.5} />
           </div>
           <div>
-            <Text>{item.todo.slice(0, 45)}</Text>
+            <p>{item.id}</p>
+            <Text>{item.todo?.slice(0, 45)}</Text>
           </div>
         </div>
       )}
     </Draggable>
   ));
 
+  const inCompleteTodosItems = inCompleteTodos.map((item, index) => (
+    <Draggable key={item.todo} index={index} draggableId={item?.todo}>
+      {(provided, snapshot) => (
+        <div
+          className={cx(classes.item, {
+            [classes.itemDragging]: snapshot.isDragging,
+          })}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+        >
+          <div {...provided.dragHandleProps} className={classes.dragHandle}>
+            <IconGripVertical size={18} stroke={1.5} />
+          </div>
+          <div>
+            <p>{item.id}</p>
+            <Text>{item.todo?.slice(0, 45)}</Text>
+          </div>
+        </div>
+      )}
+    </Draggable>
+  ));
+
+  function moveStuff(
+    sourceHandlers: UseListStateHandlers<Todo>,
+    destinationHandlers: UseListStateHandlers<Todo>,
+    source: DraggableLocation<string>,
+    destination: DraggableLocation<string>
+  ) {
+    destinationHandlers.insert(
+      Number(destination?.index),
+      (destination.droppableId === "inCompleteList"
+        ? completedTodos
+        : inCompleteTodos)[Number(source.index)]
+    );
+    sourceHandlers.remove(source.index, 1);
+  }
+
   return (
     <DragDropContext
-      onDragEnd={({ destination, source }) =>
-        handlers.reorder({ from: source.index, to: destination?.index || 0 })
-      }
+      onDragEnd={({ destination, source }) => {
+        if (!destination) return;
+        if (source.droppableId === destination?.droppableId) {
+          return completedTodoshandlers.reorder({
+            from: source.index,
+            to: destination?.index || 0,
+          });
+        } else {
+          if (source.droppableId === "inCompleteList") {
+            moveStuff(
+              inCompleteTodoshandlers,
+              completedTodoshandlers,
+              source,
+              destination
+            );
+          } else {
+            moveStuff(
+              completedTodoshandlers,
+              inCompleteTodoshandlers,
+              source,
+              destination
+            );
+          }
+          // completedTodoshandlers.remove(source.index, 1);
+          // inCompleteTodoshandlers.insert(
+          //   Number(destination?.index),
+          //   completedTodos[Number(destination?.index)]
+          // );
+        }
+      }}
     >
-      <Droppable droppableId="dnd-list" direction="vertical">
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {items}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
+      <Group gap="xl">
+        <Paper withBorder p="lg">
+          <Stack>
+            <Title>Completed</Title>
+            <Droppable droppableId="completedList" direction="vertical">
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {completedTodosItems}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>{" "}
+          </Stack>
+        </Paper>
+
+        <Paper withBorder p="lg">
+          <Stack>
+            <Title>InComplete</Title>
+            <Droppable droppableId="inCompleteList" direction="vertical">
+              {(provided) => (
+                <div {...provided.droppableProps} ref={provided.innerRef}>
+                  {inCompleteTodosItems}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </Stack>
+        </Paper>
+      </Group>
     </DragDropContext>
   );
 }
