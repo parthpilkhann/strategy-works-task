8cafa104faefd544d8bf9cff582dc39f85e027f2
SP00419-at-675013145414 <Consultant.Parthsaini@nivabupa.com>
2025-02-19 16:02:31 +0530

    added deployment code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+// const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: "/strategy-works-task/",
+  basePath: "/strategy-works-task",
+  output: "export",
 };
 
 export default nextConfig;
