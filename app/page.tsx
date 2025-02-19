1bb518dcf9a3271aba44537abdbc1f6a2fe529ea
parthpilkhan <parthpilkhann@gmail.com>
2025-02-19 20:43:11 +0530

    Revert "minor"
    This reverts commit 0b3ba978ac8bf67b5d88afbb2c99eb1e698124e1.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
-import LargestPageContentful from "@/components/LargestPageContentful/LargestPageContentful";
+import { DndListHandle } from "@/components/DndListHandle/DndListHandle";
+import { Todo } from "@/helpers/types";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [testData, setTestData] = useState<null | Todo[]>(null);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/test")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setTestData(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div>
       <main>
-        <LargestPageContentful />
+        <DndListHandle todos={testData} />
       </main>
     </div>
   );
